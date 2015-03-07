dc78fda8277015f293bbbf7909e57e4a76455589
Bikram Chatterjee <razorpeak@gmail.com>
2015-03-07 22:48:07 +0100

    README wit xlsx structure

--- README.md
+++ README.md
@@ -15,3 +15,33 @@ erljsonxlsx:from_json(Json :: #{}) -> {ok, XlsxBin :: binary()} | {error, term()
 ```erlang
 erljsonxlsx:from_xlsx(XlsxBin :: binary()) -> {ok, Json :: #{}} | {error, term()}
 ```
+
+```erlang
+|-- [Content_Types].xml
+|-- _rels
+|-- docProps
+|   |-- app.xml
+|   `-- core.xml
+`-- xl
+    |-- _rels
+    |   `-- workbook.xml.rels
+    |-- drawings
+    |   |-- _rels
+    |   |   `-- drawing1.xml.rels
+    |   `-- drawing1.xml
+    |-- media
+    |   `-- image1.jpeg
+    |-- printerSettings
+    |   `-- printerSettings1.bin
+    |-- sharedStrings.xml
+    |-- styles.xml
+    |-- theme
+    |   `-- theme1.xml
+    |-- workbook.xml
+    `-- worksheets
+        |-- _rels
+        |   `-- sheet3.xml.rels
+        |-- sheet1.xml
+        |-- sheet2.xml
+        `-- sheet3.xml
+```
--- src/erljsonxlsx.erl
+++ src/erljsonxlsx.erl
@@ -8,7 +8,42 @@
 %% ===================================================================
 
 -spec template_json() -> Json :: #{}.
-template_json() -> #{}.
+template_json() ->
+    #{<<"root">> =>
+      [#{<<"type">> => <<"folder">>,
+         <<"name">> => <<"_rels">>,
+         <<"content">> =>
+         [#{<<"type">> => <<"file">>,
+            <<"name">> => <<".rels">>,
+            <<"content">> =>
+                <<"<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>"
+                  "<Relationships xmlns=\"http://schemas.openxmlformats.org/package/2006/relationships\">"
+                       "<Relationship Id=\"rId3\" Type=\"http://schemas.openxmlformats.org/officeDocument/2006/"
+                                                      "relationships/extended-properties\""
+                                    " Target=\"docProps/app.xml\"/>"
+                       "<Relationship Id=\"rId2\" Type=\"http://schemas.openxmlformats.org/package/2006/"
+                                                        "relationships/metadata/core-properties\""
+                                    " Target=\"docProps/core.xml\"/>"
+                       "<Relationship Id=\"rId1\" Type=\"http://schemas.openxmlformats.org/officeDocument/2006/"
+                                                      "relationships/officeDocument\""
+                                              " Target=\"xl/workbook.xml\"/>"
+                  "</Relationships>">>
+           }]
+        },
+       #{<<"type">> => <<"folder">>,
+         <<"name">> => <<"docProps">>,
+         <<"content">> => []
+        },
+       #{<<"type">> => <<"folder">>,
+         <<"name">> => <<"xl">>,
+         <<"content">> => []
+        },
+       #{<<"type">> => <<"file">>,
+         <<"name">> => <<"[Content_Types].xml">>,
+         <<"content">> => []
+        }
+      ]
+     }.
 
 -spec from_json(Json :: #{}) -> {ok, XlsxBin :: binary()} | {error, term()}.
 from_json(Json) when is_map(Json) ->
